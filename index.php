74c472c14eeca5bc931df2d57f93e22d534e1ead
daniel <daniel@histomedic.mx>
2026-08-19 18:07:20 -0600

    20260819 1807

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 //******************************** */
 // [ Versión del Sistema ]
-const VERSION_SYS = "1.0.22";
+const VERSION_SYS = "1.0.24";
 const DIR = __DIR__;
 
 //******************************** */
